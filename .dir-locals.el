5d50d76a7864caa993144ae6f9204e116e4302ab
Yuriy VG <yuravg@gmail.com>
2019-12-25 14:02:00 +0300

    fixup! add files for development only.

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see Emacs manual: Per-Directory Local Variables
+
+((sh-mode . ((sh-basic-offset . 2)
+             (tab-width . 2)
+             (hi-lock-file-patterns . (
+                                       ("\\_<echon\\_>"
+                                        (0
+                                         '(:inherit font-lock-function-name-face)
+                                         prepend)))))))
---
+++ install.bat
@@ -0,0 +1,1 @@
+install.cmd "C:\git"
